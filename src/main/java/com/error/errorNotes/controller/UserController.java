229da2f1592c3baea0ee6c684cccbe03b3292bee
ADIAWIAKOYE <adiawiakoye.le10@gmail.com>
2022-08-27 08:31:01 +0000

    configuration Swagger

--- src/main/java/com/error/errorNotes/controller/UserController.java
+++ src/main/java/com/error/errorNotes/controller/UserController.java
@@ -6,10 +6,13 @@ import com.error.errorNotes.model.Probleme;
 import com.error.errorNotes.model.Solution;
 import com.error.errorNotes.services.ServicesAdmins;
 import com.error.errorNotes.services.ServicesUsers;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@Api(value = "hello", description = "Sample hello world application")
 @RestController
 @RequestMapping("/user")
 @AllArgsConstructor
@@ -19,6 +22,7 @@ public class UserController {
     final private ServicesAdmins servicesAdmins;
     final private ServicesUsers servicesUsers;
 
+    @ApiOperation(value = "Just to test the sample test api of My App Service")
     @PostMapping("/createProbleme/{email}/{password}")
     public String createProbleme(@RequestBody Probleme probleme, @PathVariable String email, @PathVariable String password){
         if (servicesUsers.connexion(email, password)){
@@ -28,6 +32,7 @@ public class UserController {
         return "Acces refusé";
     }
 
+    @ApiOperation(value = "Just to test the sample test api of My App Service")
     @PostMapping("/createSolution/{email}/{password}")
     public String createSolution(@RequestBody Solution solution, @PathVariable String email, @PathVariable String password){
         if(servicesUsers.connexion(email, password)) {
@@ -37,6 +42,7 @@ public class UserController {
         return "Acces refusé";
     }
 
+    @ApiOperation(value = "Just to test the sample test api of My App Service")
     @PostMapping("/createCommentaire/{email}/{password}")
     public String createCommentaire(@RequestBody Commentaire commentaire, @PathVariable String email, @PathVariable String password){
         if(servicesUsers.connexion(email, password)) {
--- src/main/java/com/error/errorNotes/services/ServicesAdmins.java
+++ src/main/java/com/error/errorNotes/services/ServicesAdmins.java
@@ -4,7 +4,9 @@ package com.error.errorNotes.services;
 import com.error.errorNotes.model.Etat;
 import com.error.errorNotes.model.Technologie;
 import com.error.errorNotes.model.Utilisateur;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface ServicesAdmins{
 
     Etat creerEtat(Etat etat);
